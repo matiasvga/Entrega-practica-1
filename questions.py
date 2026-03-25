690d215ce96cb5c0b1991959cb0c06d72febaffa
Matias Vega <vegamatiascorreo@gmail.com>
2026-03-25 18:59:57 -0300

    ahora el programa verifica si el caracter ingresado es válido o no

--- questions.py
+++ questions.py
@@ -1,4 +1,9 @@
 import random
+import string
+
+ascii_string = string.ascii_lowercase
+ascii_list = [x for x in ascii_string]
+
 words = [
     "python", "programa", "variable", "funcion",
     "bucle", "cadena", "entero", "lista",
@@ -6,7 +11,7 @@ words = [
 
 word = random.choice(words)
 guessed = []
-attempts = 0
+attempts = 6
 print("¡Bienvenido al Ahorcado!")
 print()
 while attempts > 0:
@@ -17,15 +22,18 @@ while attempts > 0:
             progress += letter + " "
         else:
             progress += "_ "
-
+            
     print(progress)
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print("¡Ganaste!")
         break
+
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
     letter = input("Ingresá una letra: ")
+    while not (letter in ascii_list):
+        letter = input("Caracter inválido. Ingresar otro: ")
     if letter in guessed:
         print("Ya usaste esa letra.")
     elif letter in word:
@@ -35,6 +43,7 @@ while attempts > 0:
         guessed.append(letter)
         attempts -= 1
         print("Esa letra no está en la palabra.")
+
     print()
 else:
     print(f"¡Perdiste! La palabra era: {word}")
